713745bbcd3a51862979f4a5fbbeaed33c0118d4
Sweety Jangale <sweety.jangale@indexnine.com>
2021-06-03 16:31:41 +0530

    second commit

---
+++ src/components/Context.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react'
+
+export const DataContext = React.createContext();
+
+export class DataProvider extends Component {
+
+    state = {
+        products: [
+            {
+                "_id": "1",
+                "title": "Nike Shoes 01",
+                "src": "https://cdn.shopclues.com/images1/thumbnails/105192/320/320/149110017-105192149-1588784160.jpg",
+                "price": 23,
+                "colors": ["red", "black", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "2",
+                "title": "Nike Shoes 02",
+                "src": "https://n4.sdlcdn.com/imgs/i/3/c/Nike-EPIC-REACT-FLYKNIT-Yellow-SDL108443724-1-830b0.png",
+                "price": 19,
+                "colors": ["red", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "3",
+                "title": "Nike Shoes 03",
+                "src": "https://5.imimg.com/data5/HW/EU/FE/IOS-12048161/product-jpeg-500x500.png",
+                "price": 50,
+                "colors": ["lightblue", "white", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "4",
+                "title": "Nike Shoes 04",
+                "src": "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13762630/2021/4/26/c37680f4-f21a-419d-89f9-7e6e3bdb53481619433871052-Nike-Revolution-5-Womens-Running-Shoe-3711619433870566-1.jpg",
+                "price": 15,
+                "colors": ["orange", "black", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "5",
+                "title": "Nike Shoes 05",
+                "src": "https://n3.sdlcdn.com/imgs/i/z/e/Nike-White-Running-Shoes-SDL421609343-1-f9393.jpg",
+                "price": 10,
+                "colors": ["orange", "black", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "6",
+                "title": "Nike Shoes 06",
+                "src": "https://cdn.runrepeat.com/i/nike/27837/nike-free-rn-2018-zapatillas-de-entrenamiento-para-hombre-gris-gris-loup-blanc-vert-volt-blanc-003-40-eu-hombre-gris-gris-loup-blanc-vert-volt-blanc-003-bd84-600.jpg",
+                "price": 17,
+                "colors": ["orange", "black", "crimson", "teal"],
+                "count": 1
+            }
+        ],
+        cart: [],
+        total: 0
+        
+    };
+
+    addCart = (id) =>{
+        const {products, cart} = this.state;
+        const check = cart.every(item =>{
+            return item._id !== id
+        })
+        if(check){
+            const data = products.filter(product =>{
+                return product._id === id
+            })
+            this.setState({cart: [...cart,...data]})
+        }else{
+            alert("The product has been added to cart.")
+        }
+    };
+
+    reduction = id =>{
+        const { cart } = this.state;
+        cart.forEach(item =>{
+            if(item._id === id){
+                item.count === 1 ? item.count = 1 : item.count -=1;
+            }
+        })
+        this.setState({cart: cart});
+        this.getTotal();
+    };
+
+    increase = id =>{
+        const { cart } = this.state;
+        cart.forEach(item =>{
+            if(item._id === id){
+                item.count += 1;
+            }
+        })
+        this.setState({cart: cart});
+        this.getTotal();
+    };
+
+    removeProduct = id =>{
+        if(window.confirm("Do you want to delete this product?")){
+            const {cart} = this.state;
+            cart.forEach((item, index) =>{
+                if(item._id === id){
+                    cart.splice(index, 1)
+                }
+            })
+            this.setState({cart: cart});
+            this.getTotal();
+        }
+       
+    };
+
+    getTotal = ()=>{
+        const{cart} = this.state;
+        const res = cart.reduce((prev, item) => {
+            return prev + (item.price * item.count);
+        },0)
+        this.setState({total: res})
+    };
+    
+    componentDidUpdate(){
+        localStorage.setItem('dataCart', JSON.stringify(this.state.cart))
+        localStorage.setItem('dataTotal', JSON.stringify(this.state.total))
+    };
+
+    componentDidMount(){
+        const dataCart = JSON.parse(localStorage.getItem('dataCart'));
+        if(dataCart !== null){
+            this.setState({cart: dataCart});
+        }
+        const dataTotal = JSON.parse(localStorage.getItem('dataTotal'));
+        if(dataTotal !== null){
+            this.setState({total: dataTotal});
+        }
+    }
+   
+
+    render() {
+        const {products, cart,total} = this.state;
+        const {addCart,reduction,increase,removeProduct,getTotal} = this;
+        return (
+            <DataContext.Provider 
+            value={{products, addCart, cart, reduction,increase,removeProduct,total,getTotal}}>
+                {this.props.children}
+            </DataContext.Provider>
+        )
+    }
+}
