fed6c0c8eee1ff0139bfef5a78cb33f892254377
Sweety Jangale <sweety.jangale@indexnine.com>
2021-06-03 15:34:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import Header from './components/Header'
+import Section from './components/Section'
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="app">
+        <Router>
+          <Header/>
+          <Section/>
+        </Router>
+      
+      </div>
+    )
+  }
+}
+
+export default App;
